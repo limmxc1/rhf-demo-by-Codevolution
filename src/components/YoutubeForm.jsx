a12bc4e2de67e8a1d03c330d84c1420d60f69269
Cheval Lim <limmxc1@gmail.com>
2023-12-27 22:06:11 +0800

    Form Setup

---
+++ src/components/YoutubeForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const YoutubeForm = () => {
+	return (
+		<div>
+			<form>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					id="username"
+					name="username"
+				></input>
+				<label htmlFor="email">E-mail</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+				></input>
+				<label htmlFor="channel">Channel</label>
+				<input
+					type="text"
+					id="channel"
+					name="channel"
+				></input>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
+};
